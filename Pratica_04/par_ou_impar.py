f537b0eff68fcb8d99beffbb69315f11e3434ed0
Lucas Freitas <devlucao@gmail.com>
2025-08-27 21:23:55 -0300

    adiciona funcao par ou impar

---
+++ Pratica_04/par_ou_impar.py
@@ -0,0 +1,19 @@
+pares = []
+impares = []
+
+try:
+  numero = int(input("Digite um número inteiro (ou fim para sair): "))
+  while True:
+    if numero == 'fim':
+            break
+    
+    if numero % 2 == 0:
+      pares.append(numero)
+    else:
+      impares.append(numero)
+    numero = int(input("Digite outro número inteiro (ou fim para sair): "))
+except ValueError:
+  print(f"Total de números pares: {len(pares)}")
+  print(f"Total de números ímpares: {len(impares)}")
+
+
